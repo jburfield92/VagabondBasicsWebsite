7d6feb08abcb731156a0cfa47cd5d120693ce722
jburfield92 <jburfield@knights.ucf.edu>
2016-11-19 21:35:56 -0500

    Initial Project Creation
    Created MVC ASP.NET Template Project

---
+++ VagabondBasics/VagabondBasics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VagabondBasics.Startup))]
+namespace VagabondBasics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
